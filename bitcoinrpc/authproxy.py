f4937cc282f8272101d027f31b7b1bee34ac5b9f
Nicolas Sandller <nicosandller@gmail.com>
2016-03-29 15:42:53 -0300

    Changed httplib to requests and tested getinfo() method. Works great :)

--- bitcoinrpc/authproxy.py
+++ bitcoinrpc/authproxy.py
@@ -34,48 +34,46 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
 
-try:
-    import http.client as httplib
-except ImportError:
-    import httplib
-import base64
-import decimal
+# try:
+#     import http.client as httplib
+# except ImportError:
+#     import httplib
+import requests
 import json
+from base64 import b64encode
+# import base64
+import decimal
 import logging
-try:
-    import urllib.parse as urlparse
-except ImportError:
-    import urlparse
+# try:
+#     import urllib.parse as urlparse
+# except ImportError:
+#     import urlparse
 
 USER_AGENT = "AuthServiceProxy/0.1"
 
-HTTP_TIMEOUT = 30
+# HTTP_TIMEOUT = 30
 
 log = logging.getLogger("BitcoinRPC")
 
-class JSONRPCException(Exception):
-    def __init__(self, rpc_error):
-        parent_args = []
-        try:
-            parent_args.append(rpc_error['message'])
-        except:
-            pass
-        Exception.__init__(self, *parent_args)
-        self.error = rpc_error
-        self.code = rpc_error['code'] if 'code' in rpc_error else None
-        self.message = rpc_error['message'] if 'message' in rpc_error else None
-
-    def __str__(self):
-        return '%d: %s' % (self.code, self.message)
-
-    def __repr__(self):
-        return '<%s \'%s\'>' % (self.__class__.__name__, self)
-
-
-def EncodeDecimal(o):
-    if isinstance(o, decimal.Decimal):
-        return float(round(o, 8))
-    raise TypeError(repr(o) + " is not JSON serializable")
+# class JSONRPCException(Exception):
+#     def __init__(self, rpc_error):
+#         parent_args = []
+#         try:
+#             parent_args.append(rpc_error['message'])
+#         except:
+#             pass
+#         Exception.__init__(self, *parent_args)
+#         self.error = rpc_error
+#         self.code = rpc_error['code'] if 'code' in rpc_error else None
+#         self.message = rpc_error['message'] if 'message' in rpc_error else None
+#
+#     def __str__(self):
+#         return '%d: %s' % (self.code, self.message)
+#
+#     def __repr__(self):
+#         return '<%s \'%s\'>' % (self.__class__.__name__, self)
+#
+#
 
 class AuthServiceProxy(object):
     __id_count = 0
@@ -139,7 +137,7 @@ class AuthServiceProxy(object):
         """
         Return an instance of AuthServiceProxy with an rpc_call defined.
         When the attribute (method) is not defined (i.e: instance.getinfo()),
-        then __getattr__ is called with the name (getinfo) as parámeter.
+        then __getattr__ is called with the name (getinfo) as parameter.
         It then calls AuthServiceProxy as a function,
         defining self.rpc_call to the attribute's name.
         """
@@ -151,35 +149,52 @@ class AuthServiceProxy(object):
         # If there is an rpc_call
         if self.__rpc_call is not None:
             name = "%s.%s" % (self.__rpc_call, name)
-        return AuthServiceProxy(self.__service_url, name, self.__timeout, self.__conn)
+        # return AuthServiceProxy(self.__service_url, name, self.__timeout, self.__conn)
+        return AuthServiceProxy(self.__rpcuser, self.__rpcpasswd,
+                                self.__rpchost,
+                                self.__rpcport,
+                                name)
 
     def __call__(self, *args):
         print 'aparecio __call__'
         print args
         AuthServiceProxy.__id_count += 1
 
-        log.debug("-%s-> %s %s"%(AuthServiceProxy.__id_count, self.__service_name,
-                                 json.dumps(args, default=EncodeDecimal)))
+        # log.debug("-%s-> %s %s" % (AuthServiceProxy.__id_count, self.__service_name,
+        #                          json.dumps(args, default=EncodeDecimal)))
         postdata = json.dumps({'version': '1.1',
-                               'method': self.__service_name,
+                               'method': self.__rpc_call,
                                'params': args,
-                               'id': AuthServiceProxy.__id_count}, default=EncodeDecimal)
-        self.__conn.request('POST', self.__url.path, postdata,
-                            {'Host': self.__url.hostname,
-                             'User-Agent': USER_AGENT,
-                             'Authorization': self.__auth_header,
-                             'Content-type': 'application/json'})
-        self.__conn.sock.settimeout(self.__timeout)
+                               'id': AuthServiceProxy.__id_count}, default=self.EncodeDecimal)
+        print 'postdata'
+        print postdata
+        # self.__conn.request('POST', self.__url.path, postdata,
+        #                     {'Host': self.__url.hostname,
+        #                      'User-Agent': USER_AGENT,
+        #                      'Authorization': self.__auth_header,
+        #                      'Content-type': 'application/json'})
+        # self.__conn.sock.settimeout(self.__timeout)
+        url = ''.join(['http://', self.__rpchost, ':', self.__rpcport])
+        print url
+        r = requests.post(url, data=postdata, headers=self.__headers)
+        # response = self._get_response()
+        if r.status_code == 200:
+            log.info("Response: %s" % r.json())
+            return r.json()['result']
+        else:
+            log.error("Error! Status code: %s" % r.status_code)
+            log.error("Text: %s" % r.text)
+            log.error("Json: %s" % r.json())
+            return r.json()
 
-        response = self._get_response()
-        if 'error' in response:
-            if response['error'] is not None:
-                raise JSONRPCException(response['error'])
-        elif 'result' not in response:
-            raise JSONRPCException({
-                'code': -343, 'message': 'missing JSON-RPC result'})
-
-        return response['result']
+        # if 'error' in response:
+        #     if response['error'] is not None:
+        #         raise JSONRPCException(response['error'])
+        # elif 'result' not in response:
+        #     raise JSONRPCException({
+        #         'code': -343, 'message': 'missing JSON-RPC result'})
+        #
+        # return response['result']
 
     def batch_(self, rpc_calls):
         """Batch RPC call.
@@ -211,16 +226,21 @@ class AuthServiceProxy(object):
                 results.append(response['result'])
         return results
 
-    def _get_response(self):
-        http_response = self.__conn.getresponse()
-        if http_response is None:
-            raise JSONRPCException({
-                'code': -342, 'message': 'missing HTTP response from server'})
+    def EncodeDecimal(o):
+        if isinstance(o, decimal.Decimal):
+            return float(round(o, 8))
+        raise TypeError(repr(o) + " is not JSON serializable")
 
-        responsedata = http_response.read().decode('utf8')
-        response = json.loads(responsedata, parse_float=decimal.Decimal)
-        if "error" in response and response["error"] is None:
-            log.debug("<-%s- %s"%(response["id"], json.dumps(response["result"], default=EncodeDecimal)))
-        else:
-            log.debug("<-- "+responsedata)
-        return response
+    # def _get_response(self):
+    #     http_response = self.__conn.getresponse()
+    #     if http_response is None:
+    #         raise JSONRPCException({
+    #             'code': -342, 'message': 'missing HTTP response from server'})
+    #
+    #     responsedata = http_response.read().decode('utf8')
+    #     response = json.loads(responsedata, parse_float=decimal.Decimal)
+    #     if "error" in response and response["error"] is None:
+    #         log.debug("<-%s- %s"%(response["id"], json.dumps(response["result"], default=EncodeDecimal)))
+    #     else:
+    #         log.debug("<-- "+responsedata)
+    #     return response
